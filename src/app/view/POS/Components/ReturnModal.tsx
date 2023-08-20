25f2eced4e3574165d355f29c0bb53188abe1fed
Borhan Uddin <borhan.dev@gmail.com>
2023-08-20 22:56:20 +0600

    Some Changes Happened

--- src/app/view/POS/Components/ReturnModal.tsx
+++ src/app/view/POS/Components/ReturnModal.tsx
@@ -1,14 +1,16 @@
-import { Modal, Button, Table } from 'antd';
-import { Formik, Form } from 'formik';
-import React, { SetStateAction, useEffect, useState } from 'react';
-import { useAppDispatch, useTypedSelector } from '../../../../redux/store';
-import { toast } from 'react-toastify';
-import { CommonInput, SelectInput } from '../../../components';
-import { AiFillCloseCircle, AiOutlineClose } from 'react-icons/ai';
-import api from '../../../../api';
-import { rejectedToast } from '../../../utils/toaster';
-import { fetchProduct } from '../../../../redux/actions/product';
-import { getInvoice } from '../../../../redux/actions/invoice';
+import { Modal, Button, Table } from "antd";
+import { Formik, Form } from "formik";
+import React, { SetStateAction, useEffect, useState } from "react";
+import { useAppDispatch, useTypedSelector } from "../../../../redux/store";
+import { toast } from "react-toastify";
+import { CommonInput, SelectInput } from "../../../components";
+import { AiFillCloseCircle, AiOutlineClose } from "react-icons/ai";
+import api from "../../../../api";
+import { rejectedToast } from "../../../utils/toaster";
+import { fetchProduct } from "../../../../redux/actions/product";
+import { getInvoice } from "../../../../redux/actions/invoice";
+import { fetchCustomer } from "../../../../redux/actions/customer";
+import { fetchEmployee } from "../../../../redux/actions/employee";
 
 interface ReturnModalProps {
   showReturnModal: boolean;
@@ -25,20 +27,20 @@ const ReturnModal: React.FC<ReturnModalProps> = ({
   returnId,
   setReturnId,
   setReturnLoading,
-  setReturns
+  setReturns,
 }) => {
-  const { customers } = useTypedSelector(state => state.customer);
-  const { products } = useTypedSelector(state => state.products);
+  const { customers } = useTypedSelector((state) => state.customer);
+  const { products } = useTypedSelector((state) => state.products);
   const [returnProduct, setReturnProduct] = useState<Product[]>([]);
 
   const dispatch = useAppDispatch();
 
-  const [customerPhone, setCustomerPhone] = useState('');
+  const [customerPhone, setCustomerPhone] = useState("");
 
   useEffect(() => {
     if (returnProduct.length > 0) {
-      const customer = customers.find(c => {
-        return c.purchasedProducts.some(p => {
+      const customer = customers.find((c) => {
+        return c.purchasedProducts.some((p) => {
           return p.itemCode === returnProduct[0].itemCode;
         });
       });
@@ -59,27 +61,27 @@ const ReturnModal: React.FC<ReturnModalProps> = ({
         onCancel={() => setShowReturnModal(false)}
       >
         <Formik
-          initialValues={{ itemCode: '' }}
+          initialValues={{ itemCode: "" }}
           onSubmit={(values, { resetForm }) => {
             const returnProduct = products.filter(
-              product =>
-                product.sellingStatus === 'Sold' &&
+              (product) =>
+                product.sellingStatus === "Sold" &&
                 product.itemCode === values.itemCode
             );
 
             if (returnProduct.length) {
-              setReturnProduct(prev => [...prev, ...returnProduct]);
+              setReturnProduct((prev) => [...prev, ...returnProduct]);
             } else {
-              toast.error('Maybed Product not selled');
+              toast.error("Maybed Product not selled");
             }
             resetForm();
           }}
         >
           <Form>
             <CommonInput
-              placeholder='Enter Product Code'
-              label='Item Code'
-              name='itemCode'
+              placeholder="Enter Product Code"
+              label="Item Code"
+              name="itemCode"
             />
           </Form>
         </Formik>
@@ -90,21 +92,21 @@ const ReturnModal: React.FC<ReturnModalProps> = ({
               rowKey={(obj: Product) => obj.itemCode}
               pagination={false}
             >
-              <Table.Column title='SL' render={(_, rec, index) => index + 1} />
-              <Table.Column title='Item Code' dataIndex={'itemCode'} />
+              <Table.Column title="SL" render={(_, rec, index) => index + 1} />
+              <Table.Column title="Item Code" dataIndex={"itemCode"} />
               <Table.Column
-                title='Price'
-                dataIndex={'sellPriceAfterDiscount'}
+                title="Price"
+                dataIndex={"sellPriceAfterDiscount"}
               />
               <Table.Column
                 title={<AiFillCloseCircle />}
                 render={(_, rec: Product) => {
                   return (
                     <AiOutlineClose
-                      cursor={'pointer'}
+                      cursor={"pointer"}
                       onClick={() => {
                         const filteredReturn = returnProduct.filter(
-                          p => p.id !== rec.id
+                          (p) => p.id !== rec.id
                         );
 
                         setReturnProduct(filteredReturn);
@@ -118,57 +120,59 @@ const ReturnModal: React.FC<ReturnModalProps> = ({
             <Formik
               enableReinitialize
               initialValues={{
-                items: returnProduct.map(item => item.itemCode),
-                check: '',
-                exchange: '',
+                items: returnProduct.map((item) => item.itemCode),
+                check: "",
+                exchange: "",
                 customerPhone: customerPhone,
                 cash: 0,
                 bkash: 0,
                 cbl: 0,
-                returnDate: ''
+                returnDate: "",
               }}
               onSubmit={(values, { resetForm }) => {
                 setReturnLoading(true);
                 api
-                  .post('/invoice/return', values)
-                  .then(res => {
+                  .post("/invoice/return", values)
+                  .then((res) => {
                     if (res.data.exchange) {
                       setReturnId(res.data.id);
-                      api.get('/invoice/return').then(res => {
+                      api.get("/invoice/return").then(async (res) => {
                         setReturns(res.data);
                       });
                     } else {
                       setReturnId(null);
                     }
+                    toast.success("Product Successfully Returned");
                   })
-                  .catch(err => {
+                  .catch((err) => {
                     rejectedToast(err);
                   })
-                  .finally(() => {
-                    dispatch(getInvoice());
-                    dispatch(fetchProduct());
+                  .finally(async () => {
+                    await dispatch(getInvoice());
+                    await dispatch(fetchProduct());
+                    await dispatch(fetchEmployee());
+                    await dispatch(fetchCustomer());
                     resetForm();
                     setShowReturnModal(false);
                     setReturnLoading(false);
-                    toast.success('Product Successfully Returned');
                   });
               }}
             >
               {({ values, setFieldValue, handleSubmit, handleChange }) => (
-                <Form className='mt-2 flex flex-col gap-y-3'>
+                <Form className="mt-2 flex flex-col gap-y-3">
                   <CommonInput
-                    label='Check %'
-                    name='check'
-                    placeholder='eg: 100%'
+                    label="Check %"
+                    name="check"
+                    placeholder="eg: 100%"
                     onChange={handleChange}
                   />
                   <SelectInput
-                    label='Exchange'
-                    name='exchange'
-                    placeholder='Exchange'
+                    label="Exchange"
+                    name="exchange"
+                    placeholder="Exchange"
                     value={values.exchange}
                     onChange={handleChange}
-                    children={['Exchanging', 'Not Exchanging'].map(e => (
+                    children={["Exchanging", "Not Exchanging"].map((e) => (
                       <option value={e} key={e}>
                         {e}
                       </option>
@@ -177,25 +181,25 @@ const ReturnModal: React.FC<ReturnModalProps> = ({
 
                   {!customerPhone && (
                     <CommonInput
-                      name='customerPhone'
+                      name="customerPhone"
                       value={customerPhone}
                       disabled={!!customerPhone}
-                      label='Customer Phone'
+                      label="Customer Phone"
                     />
                   )}
 
-                  {values.exchange === 'Not Exchanging' ? (
+                  {values.exchange === "Not Exchanging" ? (
                     <>
                       <CommonInput
-                        name='returnDate'
-                        placeholder='Date'
-                        label='Date'
-                        type='date'
+                        name="returnDate"
+                        placeholder="Date"
+                        label="Date"
+                        type="date"
                         step={1}
                       />
 
-                      <h1 className='text-xl font-semibold'>
-                        You Have To Return Maximumn{' '}
+                      <h1 className="text-xl font-semibold">
+                        You Have To Return Maximumn{" "}
                         {returnProduct.reduce(
                           (acc, a) => acc + a.sellPriceAfterDiscount,
                           0
@@ -203,32 +207,32 @@ const ReturnModal: React.FC<ReturnModalProps> = ({
                         tk
                       </h1>
 
-                      <div className='flex gap-x-2'>
+                      <div className="flex gap-x-2">
                         <CommonInput
-                          name='cash'
-                          placeholder='CASH'
-                          label='CASH'
-                          type='number'
+                          name="cash"
+                          placeholder="CASH"
+                          label="CASH"
+                          type="number"
                           step={1}
                         />
                         <CommonInput
-                          name='bkash'
-                          placeholder='Bkash'
-                          label='Bkash'
-                          type='number'
+                          name="bkash"
+                          placeholder="Bkash"
+                          label="Bkash"
+                          type="number"
                           step={1}
                         />
                         <CommonInput
-                          name='cbl'
-                          placeholder='CBL'
-                          label='CBL'
-                          type='number'
+                          name="cbl"
+                          placeholder="CBL"
+                          label="CBL"
+                          type="number"
                           step={1}
                         />
                       </div>
                     </>
                   ) : (
-                    ''
+                    ""
                   )}
 
                   <Button onClick={() => handleSubmit()}>Return</Button>
@@ -237,7 +241,7 @@ const ReturnModal: React.FC<ReturnModalProps> = ({
             </Formik>
           </>
         ) : (
-          ''
+          ""
         )}
       </Modal>
     </>
--- src/app/view/Purchase/Purchase.tsx
+++ src/app/view/Purchase/Purchase.tsx
@@ -14,26 +14,35 @@ const Purchase: React.FC<PurchaseProps> = () => {
       await dispatch(fetchPurchase());
     })();
   }, [dispatch]);
-  const { purchases, isLoading } = useTypedSelector(state => state.purchase);
+  const { purchases, isLoading } = useTypedSelector((state) => state.purchase);
+
   return (
-    <PrintAbleLayout title={'Purchases'} showPrint={false} showPDF={false} showExcel={false}>
-      <Table dataSource={purchases} loading={isLoading} rowClassName={'dark:bg-slate-700 dark:text-white dark:hover:text-primaryColor-900'}>
+    <PrintAbleLayout
+      title={"Purchases"}
+      showPrint={false}
+      showPDF={false}
+      showExcel={false}
+    >
+      <Table
+        dataSource={purchases.filter((p) => p.invoiceNo)}
+        loading={isLoading}
+        rowClassName={
+          "dark:bg-slate-700 dark:text-white dark:hover:text-primaryColor-900"
+        }
+      >
         <Table.Column
-          title='SL'
+          title="SL"
           render={(_, rec: Purchase, index) => index + 1}
         />
-        <Table.Column title='Invoice No' dataIndex={'invoiceNo'} />
-        <Table.Column title='Supplier' dataIndex={'supplierName'} />
-        <Table.Column title='Amount' dataIndex={'purchaseAmount'} />
-        <Table.Column title='Status' dataIndex={'purchaseStatus'} />
+        <Table.Column title="Invoice No" dataIndex={"invoiceNo"} />
+        <Table.Column title="Supplier" dataIndex={"supplierName"} />
+        <Table.Column title="Amount" dataIndex={"purchaseAmount"} />
+        <Table.Column title="Status" dataIndex={"purchaseStatus"} />
+        <Table.Column title="QTY" dataIndex={"quantity"} />
         <Table.Column
-          title='QTY'
-          dataIndex={'quantity'}
-        />
-        <Table.Column
-          title='Date'
+          title="Date"
           render={(_, rec: Purchase) =>
-            moment(rec.createdAt).format('DD-MM-YYYY')
+            moment(rec.createdAt).format("DD-MM-YYYY")
           }
         />
       </Table>
